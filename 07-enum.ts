074adc34c2e7151775ed55a44fe966bd90d073c6
yaoxingyu <yaoxingyu@hydsoft.com>
2019-04-18 13:32:45 +0800

    07 枚举

--- 07-enum.ts
+++ 07-enum.ts
@@ -1,4 +1,122 @@
 // common
 {
-  
+  enum Exm {
+    a,
+    b,
+    c
+  }
+  console.log(Exm)
+}
+
+// 数字
+{
+  enum Exm {
+    a = 2,
+    b,
+    c = 1,
+    d
+  }
+  console.log(Exm)
+}
+
+// 字符串枚举
+{
+  enum Exm {
+    a = 'A',
+    b = 'B',
+    // c  // error
+  }
+  console.log(Exm)
+}
+
+// 异构枚举
+{
+  enum Exm {
+    a,
+    b = 'B'
+  }
+  console.log(Exm)
+}
+
+// 计算的和常量成员
+{
+  enum Exm {
+    a,
+    b    = 1 << 1,
+    c   = 1 << 2,
+    d  = b | c,
+    e = "123".length
+  }
+  console.log(Exm)
+}
+
+// 联合枚举与枚举成员的类型
+{
+  enum Exm {
+    Target1,
+    Target2
+  }
+  interface Target1 {
+    a: Exm.Target1
+    b: number
+  }
+  interface Target2 {
+    x: Exm.Target2
+    y: string
+  }
+  let exm1: Target1
+  let exm2: Target2
+  exm1 = {
+    a: Exm.Target1,
+    b: 5
+  }
+  exm2 = {
+    x: Exm.Target2,
+    y: 'y'
+  }
+  console.log(exm1, exm2)
+
+  function f(x: Exm):void {
+    /* if(x !== Exm.Target1 || x !== Exm.Target2) {
+    } */ // error
+    console.log(x)
+  }
+  f(4)
+}
+
+// 运行时的枚举
+{
+  enum Exm {
+    x, y, z
+  }
+  function f(o: { x: number }): number {
+    return o.x
+  }
+  f(Exm)
+}
+
+// 反向映射
+{
+  enum Exm {
+    a
+  }
+  let a: Exm.a = 1
+  console.log(Exm[a])
+}
+
+// const 枚举
+{
+  const enum Exm {
+    a = 1,
+    b = a * 2,
+    c
+  }
+  console.log([Exm.a, Exm.b, Exm.c])
+}
+
+// 外部枚举
+declare enum Exm {
+  a = 1,
+  b,
+  c = 4
 }
--- main/07-enum.js
+++ main/07-enum.js
@@ -1,4 +1,104 @@
 "use strict";
 // common
 {
+    let Exm;
+    (function (Exm) {
+        Exm[Exm["a"] = 0] = "a";
+        Exm[Exm["b"] = 1] = "b";
+        Exm[Exm["c"] = 2] = "c";
+    })(Exm || (Exm = {}));
+    console.log(Exm);
+}
+// 数字
+{
+    let Exm;
+    (function (Exm) {
+        Exm[Exm["a"] = 2] = "a";
+        Exm[Exm["b"] = 3] = "b";
+        Exm[Exm["c"] = 1] = "c";
+        Exm[Exm["d"] = 2] = "d";
+    })(Exm || (Exm = {}));
+    console.log(Exm);
+}
+// 字符串枚举
+{
+    let Exm;
+    (function (Exm) {
+        Exm["a"] = "A";
+        Exm["b"] = "B";
+        // c  // error
+    })(Exm || (Exm = {}));
+    console.log(Exm);
+}
+// 异构枚举
+{
+    let Exm;
+    (function (Exm) {
+        Exm[Exm["a"] = 0] = "a";
+        Exm["b"] = "B";
+    })(Exm || (Exm = {}));
+    console.log(Exm);
+}
+// 计算的和常量成员
+{
+    let Exm;
+    (function (Exm) {
+        Exm[Exm["a"] = 0] = "a";
+        Exm[Exm["b"] = 2] = "b";
+        Exm[Exm["c"] = 4] = "c";
+        Exm[Exm["d"] = 6] = "d";
+        Exm[Exm["e"] = "123".length] = "e";
+    })(Exm || (Exm = {}));
+    console.log(Exm);
+}
+// 联合枚举与枚举成员的类型
+{
+    let Exm;
+    (function (Exm) {
+        Exm[Exm["Target1"] = 0] = "Target1";
+        Exm[Exm["Target2"] = 1] = "Target2";
+    })(Exm || (Exm = {}));
+    let exm1;
+    let exm2;
+    exm1 = {
+        a: Exm.Target1,
+        b: 5
+    };
+    exm2 = {
+        x: Exm.Target2,
+        y: 'y'
+    };
+    console.log(exm1, exm2);
+    function f(x) {
+        /* if(x !== Exm.Target1 || x !== Exm.Target2) {
+        } */ // error
+        console.log(x);
+    }
+    f(4);
+}
+// 运行时的枚举
+{
+    let Exm;
+    (function (Exm) {
+        Exm[Exm["x"] = 0] = "x";
+        Exm[Exm["y"] = 1] = "y";
+        Exm[Exm["z"] = 2] = "z";
+    })(Exm || (Exm = {}));
+    function f(o) {
+        return o.x;
+    }
+    f(Exm);
+}
+// 反向映射
+{
+    let Exm;
+    (function (Exm) {
+        Exm[Exm["a"] = 0] = "a";
+    })(Exm || (Exm = {}));
+    let a = 1;
+    console.log(Exm[a]);
+}
+// const 枚举
+{
+    console.log([1 /* a */, 2 /* b */, 3 /* c */]);
 }
